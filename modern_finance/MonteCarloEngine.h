908bdec4be23ef7ef8178d5a2bde9d66215cbe4b
Tom Haley <tomhaley2015@gmail.com>
2026-07-05 08:50:13 -0400

    Monte Carlo moved Generic types instead of copying

--- modern_finance/MonteCarloEngine.h
+++ modern_finance/MonteCarloEngine.h
@@ -1,25 +1,30 @@
 #pragma once
 #include <cstdint>
 #include <cmath>
+#include <utility>
 
 template <typename Generator, typename Distribution, typename Simulation>
 class MonteCarloEngine {
 public:
-    MonteCarloEngine(Generator generator, Distribution distribution,  Simulation simulation)
-        : generator_(generator), distribution_(distribution), simulation_(simulation) {}
-    
-    double sample() {
-        return distribution_(generator_);
-    }
-    
+    MonteCarloEngine(Generator generator, Distribution distribution, Simulation simulation)
+    : generator_(std::move(generator)),
+      distribution_(std::move(distribution)),
+      simulation_(std::move(simulation)) {}
+
     auto Run(std::uint64_t t_paths) {
         simulation_.configure(t_paths);
+
         for (std::uint64_t i = 0; i < t_paths; ++i) {
            simulation_.process_sample(sample());
         }
+
         return simulation_.results();
     }
+
 private:
+    double sample() {
+        return distribution_(generator_);
+    }
     Generator generator_;
     Distribution distribution_;
     Simulation simulation_;
